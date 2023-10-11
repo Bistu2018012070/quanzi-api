0ec23b43899627789bb4e0f9ff1f0e24400b3f94
zhangzq <zhangzq@yinhai.com>
2023-10-11 10:57:44 +0800

    feat(API): 后端项目初始化

---
+++ apiBack/src/main/java/com/yupi/project/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.yuapicommon.model.entity.User;
+
+/**
+ * 用户 Mapper
+ *
+ * QuanZi
+ * QuanZi
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ apiBack/yuapi-client-sdk/src/main/java/com/yupi/yuapiclientsdk/model/User.java
@@ -0,0 +1,15 @@
+package com.yupi.yuapiclientsdk.model;
+
+import lombok.Data;
+
+/**
+ * 用户
+ *
+ * QuanZi
+ * QuanZi
+ */
+@Data
+public class User {
+
+    private String username;
+}
